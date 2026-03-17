54378dfe8c095e097b89e826a62a813f53efabb0
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-03-17 21:14:06 +0700

    modifikasi

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Users\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class UsersTable
@@ -14,6 +15,8 @@ class UsersTable
         return $table
             ->columns([
                 //
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
             ])
             ->filters([
                 //
