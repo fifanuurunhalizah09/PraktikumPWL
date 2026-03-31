0748c1d19aec203a64b3390ab3059149f044489e
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-03-31 07:20:03 +0700

    update

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -16,56 +16,54 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Schemas\Components\Section;
 use Filament\Support\Icons\Heroicon;
-use Filament\Forms\Components\Group;
+use Filament\Schemas\Components\Group;
 
 class PostForm
 {
     public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                //section 1 - post details
-                Section::make('Post Details')
-                -> Description('Fill in the details of the post')
-                //->icon(Heroicon::RocketLaunch)
+{
+    return $schema
+        ->components([
+            // section 1 - post details
+            Section::make("Post Details")
+                ->description("Fill in the details of the post")
+                // -> icon(Heroicon::RocketLaunch)
                 -> icon('heroicon-o-document-text')
                 ->schema([
-                TextInput::make('title'),
-                    // ->required()
-                    // ->minLength(5),
-                TextInput::make('slug'),
-                    // ->required()
-                    // ->unique(ignoreRecord: true),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->preload()
-                    ->searchable(),
-                ColorPicker::make('color'),
-                MarkdownEditor::make('content'),
-                ])->ColumnSpanFull(),
-                
-                //section 2 - image
-                Section::make('Image Upload')
-                ->schema([
-                    FileUpload::make('image')
-                        ->disk('public')
-                        ->directory('posts'),
-                ]),
-                //section 3 - meta
-                Section::make('Meta Information')
-                ->schema([
-                    TagsInput::make('tags'),
-                    Checkbox::make('published'),
-                    DateTimePicker::make('published_at'),
-                ]),
-            //     //RichEditor::make('content'),
-            //     FileUpload::make('image')
-            //         ->disk('public')
-            //         ->directory('posts'),
-            //     TagsInput::make('tags'),
-            //     Checkbox::make('published'),
-            //     DateTimePicker::make('published_at'),
+                    // grouping fields into 2 columns
+                    Group::make([
+                        TextInput::make("title"),
+                        TextInput::make("slug"),
+                        Select::make("category_id")
+                            ->relationship("category", "name")
+                            ->preload()
+                            ->searchable(),
+                        ColorPicker::make("color"),
+                    ])->columns(2),
 
-            ])->columns(2);
+                    MarkdownEditor::make("content"),
+                ])->columnSpan(2),
+
+            // Grouping fields into 2 columns
+            Group::make([
+                
+                // section 2 - image
+                Section::make("Image Upload")
+                    ->schema([
+                        FileUpload::make("image")
+                            ->disk("public")
+                            ->directory("posts"),
+                    ]),
+
+                // section 3 - meta
+                Section::make("Meta Information")
+                    ->schema([
+                        TagsInput::make("tags"),
+                        Checkbox::make("published"),
+                        DateTimePicker::make("published_at"),
+                    ]),
+            ])->columnSpan(1)
+
+        ])->columns(3);
     }
 }
