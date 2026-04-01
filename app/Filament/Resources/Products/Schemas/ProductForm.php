12908187833ee5f90b282502f8ed791118991094
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-04-02 06:06:14 +0700

    jobsheet7_praktikum1

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
+use Filament\Schemas\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Wizard::make([
+                    Step::make('Product Info')
+                        ->description('Isi Informasi Produk')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('name')
+                                    ->required(),
+                                TextInput::make('sku')
+                                    ->required(),
+                            ])->columns(2),
+                            MarkdownEditor::make('description')
+                        ]),
+                    // Step::make('Product prices')
+                    Step::make('Product Price and Stock')
+                        ->description('Isi Harga Produk')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('price')
+                                    ->required(),
+                                TextInput::make('stock')
+                                    ->required(),
+                            ])->columns(2),
+                            MarkdownEditor::make('description')
+                        ]),
+                ])->columnSpanFull(),
+            ]);
+    }
+}
